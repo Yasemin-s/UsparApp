79514fc1464a3fd2bfd63bd913095ca4a4f7314f
Yasemin-s <l2012721036@isparta.edu.tr>
2023-08-09 09:12:18 +0300

    son
    son

--- app/src/main/java/com/example/usparapplication/ConSQL.kt
+++ app/src/main/java/com/example/usparapplication/ConSQL.kt
@@ -6,31 +6,33 @@ import java.sql.Connection
 import java.sql.DriverManager
 
 
-class ConSQL {
+class ConSQL {  // Veritabanı bağlantısı için Connection nesnesini tutacak değişken.
     private var con: Connection? = null
     private lateinit var connectURL: String
 
+
     @SuppressLint("NewApi")
-    fun conclass(): Connection? {
-        val ip = "192.168.1.141"//DESKTOP-2JP7ACI\SQLEXPRESS2 isimle girince olmuyor
+    fun conclass(): Connection? {   // Bağlantıyı oluşturan fonksiyon.
+        val ip = "192.168.2.210"//DESKTOP-2JP7ACI\SQLEXPRESS2 isimle girince olmuyor
         val port = "1433"
         val db = "ARAKNE"
-        val username = "sa"
-        val password = "password1"
+        val username = "Sa"
+        val password = "Sa782455"
 
-        val a: StrictMode.ThreadPolicy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        val a: StrictMode.ThreadPolicy = StrictMode.ThreadPolicy.Builder().permitAll().build()  // Bu bölümde, ThreadPolicy izinleri vererek ağ işlemlerini ana iş parçacığından yapma izni veriyor.
         StrictMode.setThreadPolicy(a)
         println("a ifadesi : ${a}")
         try {
+            // SQL Server JDBC sürücüsünü yükler.
             Class.forName("net.sourceforge.jtds.jdbc.Driver")
-            connectURL = "jdbc:jtds:sqlserver://$ip:$port;databaseName=$db;user=$username;password=$password;"
-            con = DriverManager.getConnection(connectURL)
-            println("url  : ${connectURL}")
+            connectURL = "jdbc:jtds:sqlserver://$ip:$port;databaseName=$db;user=$username;password=$password;"  // Veritabanı bağlantı dizesini oluşturur.
+            con = DriverManager.getConnection(connectURL)   // Bağlantıyı oluşturur.
+            println("url  : ${connectURL}") // Bağlantı dizesini loglar.
         } catch (e: Exception) {
             Log.e("ConSQL", "Error: ${e.message}")
         }
         println("url : ${connectURL} burdaaaa")
-        println("con : ${con}")
+        println("con : ${con}") // Bağlantı nesnesini döndürür.
         return con
     }
 }
--- app/src/main/java/com/example/usparapplication/MainActivity.kt
+++ app/src/main/java/com/example/usparapplication/MainActivity.kt
@@ -1,11 +1,12 @@
+
 package com.example.usparapplication
 
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.sql.Connection
 import java.sql.ResultSet
@@ -19,44 +20,63 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnConnect: Button = findViewById(R.id.button)
+        val listView: ListView = findViewById(R.id.listView)
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val searchButton = findViewById<Button>(R.id.searchButton)
+
+        val data = ArrayList<String>() // Veri listesi tanımlandı
 
         btnConnect.setOnClickListener {
-            val nameTextView: TextView = findViewById(R.id.textView)
             val c = ConSQL()
             connection = c.conclass()
-            println("connection : ${connection}")
+
             if (connection != null) {
                 try {
-
-// Try bloğu içinde:
                     Log.d("MainActivity", "Bağlantı başarılı.")
-                    val sqlStatement = "Select * from INFO_KALITE_PERSONEL"
+                    val sqlStatement = "SELECT HT.ID, HT.TAR, HT.TID, TT.TIP AS TIPID1, HT.TIPID2, HT.M3, HT.KAYTAR, HT.EID, HT.CARI " +
+                            "FROM INFO_HAVA_TUKETIM HT " +
+                            "JOIN INFO_TIP_TANIM TT ON HT.ID = TT.ID"
                     val smt: Statement = connection!!.createStatement()
                     val resultSet: ResultSet = smt.executeQuery(sqlStatement)
-                    val studentNames = ArrayList<String>()
 
                     while (resultSet.next()) {
-                        val studentName: String? = resultSet.getString(3)
-                        if (studentName != null) {
-                            println("nameee : ${studentName}")
-                            studentNames.add(studentName)
-                        }
+                        val id = resultSet.getInt("ID")
+                        val tar = resultSet.getString("TAR")
+                        val tid = resultSet.getString("TID")
+                        val tipid1 = resultSet.getString("TIPID1")
+                        val tipid2 = resultSet.getString("TIPID2")
+                        val m3 = resultSet.getDouble("M3")
+                        val kaytar = resultSet.getString("KAYTAR")
+                        val eid = resultSet.getString("EID")
+                        val cari = resultSet.getString("CARI")
+
+                        val item = "ID: $id, TAR: $tar, TID: $tid, TIPID1: $tipid1, TIPID2: $tipid2, M3: $m3, KAYTAR: $kaytar, EID: $eid, CARI: $cari"
+                        data.add(item)
                     }
-                    Log.d("MainActivity", "Veriler başarılı bir şekilde alındı.")
 
-// studentNames listesi, tablodaki tüm öğrenci adlarını içerir.
-
-                    // Try bloğunun sonunda:
-                    val listView: ListView = findViewById(R.id.listView)
-                    val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, studentNames)
+                    val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
                     listView.adapter = adapter
-
 
                     connection!!.close()
                 } catch (e: Exception) {
-                    Log.e("error is", e.message.toString())
+                    Log.e("MainActivity", "Error: ${e.message}")
                 }
+            }
+        }
 
+        searchButton.setOnClickListener {
+            val searchText = searchEditText.text.toString().trim()
+
+            if (searchText.isNotEmpty()) {
+                val filteredData = data.filter { item ->
+                    item.contains(searchText, ignoreCase = true)
+                }.toMutableList()
+
+                val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, filteredData)
+                listView.adapter = adapter
+            } else {
+                val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
+                listView.adapter = adapter
             }
         }
     }
